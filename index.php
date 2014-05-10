61c11b9fcb9cfdb7f742b019a67fc68b2d753621
Owen Holloway <owen.holloway101@gmail.com>
2014-05-10 16:31:35 +1000

    removed the calendar from the homepage

--- index.php
+++ index.php
@@ -11,7 +11,9 @@ require_once $_SERVER["DOCUMENT_ROOT"].'/core/globalMethods.php';
 </head>
 
 <body>
+	<!--
 	<div class="content">
 	<iframe frameBorder="0" src="http://www.google.com/calendar/embed?src=welly%40tasrovers.com" style="width: 100%; height: 600px;"></iframe>
 	</div>
+	-->
 </body>
