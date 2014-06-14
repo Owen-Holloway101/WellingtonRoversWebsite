c6c96b83032242f7766b206058fa3aa5c7e703dd
Owen Holloway <owen.holloway101@gmail.com>
2014-06-15 00:28:15 +1000

    changed the user ... herpa derp

--- core/dbConnect.php
+++ core/dbConnect.php
@@ -1,5 +1,5 @@
 <?php
 //inits the variable $db with the connection to the server for query
 include $_SERVER["DOCUMENT_ROOT"].'/core/settings.private.php';
-$db = new mysqli('127.0.0.1','root',$sqlPassword,'tbrc_jo152');
+$db = new mysqli('127.0.0.1',$sqlUser,$sqlPassword,'tbrc_jo152');
 ?>
