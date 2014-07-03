16b37585f2d0bc0442f667d61be616f90b20aefa
Owen Holloway <owen.holloway101@gmail.com>
2014-07-03 19:46:12 +1000

    more work on login forms and system

--- user/index.php
+++ user/index.php
@@ -3,15 +3,23 @@ require_once $_SERVER["DOCUMENT_ROOT"].'/core/globalMethods.php';
 ?>
 
 <head>
-  <title>Welly Rover Crew - User</title>
-  <script type="text/javascript">
-    $(".headerBar").append(" - User");
-    setPage("user");
-  </script>
+	<title>Welly Rover Crew - User</title>
+	<script type="text/javascript">
+	$(".headerBar").append(" - User");
+	setPage("user");
+	</script>
 </head>
 
 <body>
-  <div id="content">
-    
-  </div>
+	<div class="content">
+	<button onclick="location.href='/user/login.php'">
+		Lo gin
+	</button><br>
+	<button onclick="location.href='/user/newUser.php'">
+		New User
+	</button><br>
+	<button onclick="location.href='/user/changePass.php'">
+		Change User
+	</button>
+	</div>
 </body>
