81b38e4bf44cae62f065fe307b2dd68fcb36d506
Owen Holloway <owen.holloway101@gmail.com>
2014-09-03 21:15:48 +1000

    added a links page

---
+++ links/index.php
@@ -0,0 +1,24 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"].'/core/globalMethods.php';
+?>
+
+<head>
+	<title>Welly Rover Crew - Home</title>
+	<script type="text/javascript">
+		$(".headerBar").append(" - Links");
+		setPage("links");
+	</script>
+</head>
+
+<body>
+	<div class="content">
+		<table>
+			<tr><a href="www.scouts.com.au">Scouts Australia</a></tr>
+			<tr><a href="www.rovers.com.au">Rovers Australia</a></tr>
+			<tr><a href="www.tas.scouts.com.au">Scout Tasmania</a></tr>
+			<tr><a href="www.tasrovers.com">Tasmanian Rovers</a></tr>
+			<tr><a href="www.tas.myscout.com.au">Tas MyScout</a></tr>
+			<tr><a href="www.facebook.com/WellingtonRovers">Facebook</a></tr>
+		</table>
+	</div>
+</body>
