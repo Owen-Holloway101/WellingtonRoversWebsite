0ff4432d0f977fc60df1920010206a35e53e4cb5
Owen Holloway <owen.holloway101@gmail.com>
2014-09-03 21:18:43 +1000

    fixed the links

--- links/index.php
+++ links/index.php
@@ -13,12 +13,12 @@ require_once $_SERVER["DOCUMENT_ROOT"].'/core/globalMethods.php';
 <body>
 	<div class="content">
 		<table>
-			<tr><a href="www.scouts.com.au">Scouts Australia</a><br></tr>
-			<tr><a href="www.rovers.com.au">Rovers Australia</a><br></tr>
-			<tr><a href="www.tas.scouts.com.au">Scout Tasmania</a><br></tr>
-			<tr><a href="www.tasrovers.com">Tasmanian Rovers</a><br></tr>
-			<tr><a href="www.tas.myscout.com.au">Tas MyScout</a><br></tr>
-			<tr><a href="www.facebook.com/WellingtonRovers">Facebook</a><br></tr>
+			<tr><a href="http://www.scouts.com.au">Scouts Australia</a><br></tr>
+			<tr><a href="http://www.rovers.com.au">Rovers Australia</a><br></tr>
+			<tr><a href="http://www.tas.scouts.com.au">Scout Tasmania</a><br></tr>
+			<tr><a href="http://www.tasrovers.com">Tasmanian Rovers</a><br></tr>
+			<tr><a href="http://www.tas.myscout.com.au">Tas MyScout</a><br></tr>
+			<tr><a href="http://www.facebook.com/WellingtonRovers">Facebook</a></tr>
 		</table>
 	</div>
 </body>
