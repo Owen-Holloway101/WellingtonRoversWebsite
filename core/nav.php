60ac79a35063dcff13eccaa21efbc92dfdad2bbb
Owen Holloway <owen.holloway101@gmail.com>
2014-09-03 21:16:40 +1000

    added a links to the links

--- core/nav.php
+++ core/nav.php
@@ -28,6 +28,7 @@
 <div class="nav">
 	<span onclick="location.href='/'" class="navButton">Home</span>
 	<span onclick="location.href='/calendar'" class="navButton">Calendar</span>
+	<span onclick="location.href='/links'" class="navButton">Links</span>
 	<div class="fb-like-box" data-href="https://www.facebook.com/WellingtonRovers" data-width="218" data-colorscheme="light" data-show-faces="false" data-header="true" data-stream="false" data-show-border="true"></div>
 	<div class="navSettings"><button onclick="toggleMobile()">Toggle Mobile Site</button></div>
 </div>
