819869889e7202fe07ce648df8c729980a755757
Owen Holloway <owen.holloway101@gmail.com>
2014-09-06 19:13:15 +1000

    asdf

--- links/index.php
+++ links/index.php
@@ -12,13 +12,11 @@ require_once $_SERVER["DOCUMENT_ROOT"].'/core/globalMethods.php';
 
 <body>
 	<div class="content">
-		<table>
-			<tr><a href="http://www.scouts.com.au">Scouts Australia</a><br></tr>
-			<tr><a href="http://www.rovers.com.au">Rovers Australia</a><br></tr>
-			<tr><a href="http://www.tas.scouts.com.au">Scout Tasmania</a><br></tr>
-			<tr><a href="http://www.tasrovers.com">Tasmanian Rovers</a><br></tr>
-			<tr><a href="http://www.tas.myscout.com.au">Tas MyScout</a><br></tr>
-			<tr><a href="http://www.facebook.com/WellingtonRovers">Facebook</a></tr>
-		</table>
+			<a href="http://www.scouts.com.au">Scouts Australia</a><br>
+			<a href="http://www.rovers.com.au">Rovers Australia</a><br>
+			<a href="http://www.tas.scouts.com.au">Scout Tasmania</a><br>
+			<a href="http://www.tasrovers.com">Tasmanian Rovers</a><br>
+			<a href="http://www.tas.myscout.com.au">Tas MyScout</a><br>
+			<a href="http://www.facebook.com/WellingtonRovers">Facebook</a>
 	</div>
 </body>
