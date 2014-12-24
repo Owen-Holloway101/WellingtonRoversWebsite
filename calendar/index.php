95033dea46c41d47fe53454f639e958c1e3f1b52
Owen Holloway <owen.holloway101@gmail.com>
2014-12-24 17:02:38 +1100

    changed the mobile calendar and fixed some styles

--- calendar/index.php
+++ calendar/index.php
@@ -10,7 +10,16 @@ require_once $_SERVER["DOCUMENT_ROOT"].'/core/globalMethods.php';
 </head>
 
 <body>
-	<div class="content">
-	<iframe frameBorder="0" src="http://www.google.com/calendar/embed?src=welly%40tasrovers.com" style="width: 100%; height: 600px;"></iframe>
-	</div>
+	<?php
+	if ($_COOKIE["useMobileSite"] == "false") {
+		echo "<div class=\"content\">
+			<iframe frameBorder=\"0\" src=\"http://www.google.com/calendar/embed?src=welly%40tasrovers.com\" style=\"width: 100%; height: 600px;\"></iframe>
+			</div>";
+	} else {
+		echo "<div class=\"content\">
+			Google calendar formats terribly on mobile, so here is a link to the calendar.<br>
+			<a href=\"http://www.google.com/calendar/embed?src=welly%40tasrovers.com\">Calendar link</a>
+			</div>";
+	}	
+	?>
 </body>
