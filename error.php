eb09dfe17022bc15ca36386d2d7da427d5c6d906
Owen Holloway <owen.holloway101@gmail.com>
2015-01-18 18:14:16 +1100

    added description to landing page
    edited the error page to not have a massive bug (trying to load from forum? what was I doing)
    
    put the facebook link back

--- error.php
+++ error.php
@@ -1,7 +1,7 @@
 <!--Owen Holloway, Welly Rover Crew, 2014-->
 <?php
 //This is the index for the forum all other pages after this _should_ be auto generated 
-require_once $_SERVER["DOCUMENT_ROOT"].'/forum/forumCore.php';
+require_once $_SERVER["DOCUMENT_ROOT"].'/core/globalMethods.php';
 ?>
 
 <head>
